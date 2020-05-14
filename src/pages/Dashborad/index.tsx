9fc2821869d15eba315341e7bf7d9aa32463dc2f
Victor Rosas <victor.rosas.96@hotmail.com>
2020-05-14 17:33:49 -0300

    Primeiro commit e finalização de estilização

---
+++ src/pages/Dashborad/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/33363507?s=460&u=97484dcfcd9e4ed93578f565253e10bc7f45fe06&v=4"
+            alt="Victor Rosas"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/33363507?s=460&u=97484dcfcd9e4ed93578f565253e10bc7f45fe06&v=4"
+            alt="Victor Rosas"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/33363507?s=460&u=97484dcfcd9e4ed93578f565253e10bc7f45fe06&v=4"
+            alt="Victor Rosas"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
